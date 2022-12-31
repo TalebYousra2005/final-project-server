33492740317bb075e349c4c319f5da94216e4736
Yousra <yousrataleb55@gmail.com>
2022-12-31 16:51:31 +0100

    ..

--- api/config/db.js
+++ api/config/db.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
 const { MONGO_PROD_URL } = process.env;
+const { MONGO_DEV_URL } = process.env;
 exports.connect = () => {
   mongoose
-    .connect(MONGO_PROD_URL, {
+    .connect(
+      MONGO_PROD_URL
+      // MONGO_DEV_URL
+      , {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
